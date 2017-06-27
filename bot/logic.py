8fdb832436028036d8d3317a063c0992a7b7f405
= <jim.richmond@riconco.com>
2017-06-27 10:12:58 -0700

    AddChild fixes

--- bot/logic.py
+++ bot/logic.py
@@ -145,7 +145,7 @@ def handle_add_child(event):
                     session["validated_phone"] = "1"
                     slots["phone_number"] = resp.phone_number
 
-                    validation_code = ''.join(random.SystemRandom().choice(['0','1','2','3','4','5','6','7','8','9']) for _ in range(5))
+                    validation_code = ''.join(random.SystemRandom().choice(['1','2','3','4','5','6','7','8','9']) for _ in range(5))
 
                     hashed_code =  hashlib.md5()
                     hashed_code.update("%s%s" % (validation_code, settings.SECRET_KEY ))
