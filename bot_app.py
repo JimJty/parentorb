c54a4a69294534817838625ab27bdbfcf3f6aa45
= <jim.richmond@riconco.com>
2017-07-01 12:44:17 -0700

    schedule actions

--- bot_app.py
+++ bot_app.py
@@ -1,5 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from zappa.async import task
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parentorb.settings_prod")
 application = get_wsgi_application() #init django, for models
@@ -10,4 +11,7 @@ def handler(event, context):
 
     return route_logic(event)
 
+@task
+def process_actions():
 
+    print "process actions"
