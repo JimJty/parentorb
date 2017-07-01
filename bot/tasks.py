6668f46b09cf2b2a3aa818f68bf80f305f49956c
= <jim.richmond@riconco.com>
2017-07-01 13:56:15 -0700

    schedule actions

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,3 +1,12 @@
+from zappa.async import run
+
+
 def process_action(action_id):
 
     print "processing: %s" % action_id
+
+    run('bot.tasks.process_sub_task', 9)
+
+def process_sub_task(action_id):
+
+    print "processing2: %s" % action_id
