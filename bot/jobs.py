0070f27f7bc45d280abd7e5f1a0073a24eeccc3e
= <jim.richmond@riconco.com>
2017-07-01 14:32:08 -0700

    schedule actions

--- bot/jobs.py
+++ bot/jobs.py
@@ -1,12 +1,15 @@
+from core.models import Action
 from zappa.async import run
+
+
+# def sample_job(action_id):
+#
+#     print "processing: %s" % action_id
+#
+#     #run(process_sub_task, (9,))
+
 
 
 def process_action(action_id):
 
-    print "processing: %s" % action_id
-
-    run(process_sub_task, (9,))
-
-def process_sub_task(action_id):
-
-    print "processing2: %s" % action_id
+    print "processing: %s" % Action.process_by_id(action_id)
