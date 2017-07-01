eb22428322c5d90e3aa39bc7653771e39496213a
= <jim.richmond@riconco.com>
2017-07-01 12:57:05 -0700

    schedule actions

--- bot_app.py
+++ bot_app.py
@@ -1,3 +1,5 @@
+import importlib
+import inspect
 import os
 from django.core.wsgi import get_wsgi_application
 from zappa.async import task
@@ -9,9 +11,43 @@ from bot.logic import route_logic
 
 def handler(event, context):
 
+    #for manual commands
+    if event.get('command', None):
+        whole_function = event['command']
+        app_function = import_module_and_get_function(whole_function)
+        result = run_function(app_function, event, context)
+        print("Result of %s:" % whole_function)
+        print(result)
+        return result
+
+    #else route to bot
     return route_logic(event)
 
-@task
-def process_actions():
 
-    print "process actions"
+def import_module_and_get_function(whole_function):
+    """
+    Given a modular path to a function, import that module
+    and return the function.
+    """
+    module, function = whole_function.rsplit('.', 1)
+    app_module = importlib.import_module(module)
+    app_function = getattr(app_module, function)
+    return app_function
+
+def run_function(app_function, event, context):
+    """
+    Given a function and event context,
+    detect signature and execute, returning any result.
+    """
+    args, varargs, keywords, defaults = inspect.getargspec(app_function)
+    num_args = len(args)
+    if num_args == 0:
+        result = app_function(event, context) if varargs else app_function()
+    elif num_args == 1:
+        result = app_function(event, context) if varargs else app_function(event)
+    elif num_args == 2:
+        result = app_function(event, context)
+    else:
+        raise RuntimeError("Function signature is invalid. Expected a function that accepts at most "
+                           "2 arguments or varargs.")
+    return result
