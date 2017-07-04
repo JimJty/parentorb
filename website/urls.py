f666e95024196b863f999d13528b18c768bb780f
= <jim.richmond@riconco.com>
2017-07-04 07:46:42 -0700

    website test

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import  url
 
-from website.views import home, error_test
+from website.views import home, error_test, handle_twilio
 
 urlpatterns = [
 
     url(r'^$', home, name='home'),
+
+    url(r'^handle-twilio/$', handle_twilio, name="hanlde-twilio"),
+
     url(r'^error-test/$', error_test, name="error-test"),
+
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 import boto3
 from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import render
 
 import logging
@@ -17,6 +20,18 @@ def home(request):
     }
 
     return render(request, 'front/home.html', context)
+
+def handle_twilio(request):
+
+    print request.POST
+
+    msg = None
+
+    results = {'status':1 if not msg else 0,"msg":msg,}
+
+
+    return HttpResponse(json.dumps(results), content_type='application/javascript')
+
 
 def error_test(request):
 
