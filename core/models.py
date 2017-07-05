176e66b712de8207c95afd1f37f3481be7fa3fc5
= <jim.richmond@riconco.com>
2017-07-05 09:22:38 -0700

    catch all

--- core/models.py
+++ core/models.py
@@ -382,8 +382,8 @@ class Action(models.Model):
 
         minutes, seconds = divmod((self.event_time - timezone.now()).total_seconds(), 60)
 
-        if minutes <=0 :
-            return None
+        if minutes <= 0 :
+            return 0
 
         return int(minutes) + 1
 
@@ -443,18 +443,15 @@ class Action(models.Model):
 
                 if self.reminder.kind == 100:
 
-                    msg = "%s said she is NOT ready for %s." % (
+                    if self.excuse == "no_response":
+                        msg = "%s does not appear ready for %s. She did not respond to my attempts to contact." % (
                             self.reminder.child.first_name,
                             self.reminder.for_desc,
                         )
-
-                    if self.excuse == "no_response":
-                        msg += " %s did not respond to my attempts to contact." % (
-                            self.reminder.child.first_name
-                        )
                     elif self.excuse:
-                        msg += " %s's reason: '%s'." % (
+                        msg = "%s said she is NOT ready for %s. The reason: '%s'." % (
                             self.reminder.child.first_name,
+                            self.reminder.for_desc,
                             self.excuse
                         )
 
