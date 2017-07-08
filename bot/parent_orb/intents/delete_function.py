61457cd90a6cf2cee7afd645b18bffb915cdabd9
= <jim.richmond@riconco.com>
2017-07-07 23:23:26 -0700

    delete function

--- bot/parent_orb/intents/delete_function.py
+++ bot/parent_orb/intents/delete_function.py
@@ -10,8 +10,10 @@ class Intent(BaseIntent):
         # delete_reminder
         # default
 
+        obj_selected = self.slot_value('object').lower() if self.slot_value('object') else None
+
         # delete_account
-        if self.slot_value('object') == 'account':
+        if obj_selected == 'account':
 
             if self.confirmation is None:
 
@@ -19,8 +21,15 @@ class Intent(BaseIntent):
                     case="delete_account",
                     resp_type=self.RESP_CONFIRM,
                     text="Whoa, DELETE your account?",
+                    menu_title="Confirm:",
+                    menu_buttons=[
+                        MenuButton("Yes", "yes"),
+                        MenuButton("No", "no"),
+                    ]
                 )
             elif self.confirmation is True:
+
+                self.user.delete()
 
                 return self.build_template(
                     case="delete_account",
@@ -34,6 +43,86 @@ class Intent(BaseIntent):
                     resp_type=self.RESP_CLOSE,
                     text="Your account was not deleted.",
                 )
+
+
+        # delete_child
+        if obj_selected == 'child':
+
+            if not self.slot_value('object_id'):
+
+                children = self.user.get_children()
+                if children.count() == 0:
+                    return self.build_template(
+                        case="delete_child",
+                        resp_type=self.RESP_CLOSE,
+                        text="You have no children setup in your account.",
+                    )
+                else:
+
+                    msg = "Which number:"
+                    counter = 1
+                    child_list = [c.id for c in children]
+                    self.set_session_value('child_list', child_list)
+
+                    for c in children:
+                        msg+= "\n %s) %s" % (counter, c.first_name)
+                        counter += 1
+
+                    return self.build_template(
+                        case="delete_child",
+                        resp_type=self.RESP_SLOT,
+                        slot="object_id",
+                        text=msg,
+                    )
+
+            else:
+
+                if self.session_value("child_list"):
+
+                    try:
+                        selected_child = self.session_value("child_list")[int(self.slot_value('object_id'))]
+                        selected_child = self.user.get_child_by_id(selected_child)
+                    except:
+                        selected_child = None
+
+                    if not selected_child:
+                        return self.build_template(
+                            case="delete_child",
+                            resp_type=self.RESP_CLOSE,
+                            text="Child not found.",
+                        )
+
+                    if self.confirmation is None:
+
+                        return self.build_template(
+                            case="delete_child",
+                            resp_type=self.RESP_CONFIRM,
+                            text="Are you sure you want to delete %s's account?" % selected_child.first_name,
+                            menu_title="Confirm:",
+                            menu_buttons=[
+                                MenuButton("Yes", "yes"),
+                                MenuButton("No", "no"),
+                            ]
+                        )
+                    elif self.confirmation is True:
+
+                        selected_child.delete()
+
+                        return self.build_template(
+                            case="delete_child",
+                            resp_type=self.RESP_CLOSE,
+                            text="Poof, child account deleted.",
+                        )
+                    else:
+
+                        return self.build_template(
+                            case="delete_child",
+                            resp_type=self.RESP_CLOSE,
+                            text="Nothing was deleted.",
+                        )
+
+
+
 
 
         # default
