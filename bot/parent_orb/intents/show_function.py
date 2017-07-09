5c30678d8d1b2148fd03a7d216f3a87635015a10
= <jim.richmond@riconco.com>
2017-07-09 06:19:22 -0700

    show function

--- bot/parent_orb/intents/show_function.py
+++ bot/parent_orb/intents/show_function.py
@@ -12,7 +12,7 @@ class Intent(BaseIntent):
         obj_selected = self.slot_value('object').lower() if self.slot_value('object') else None
 
         #list_reminders
-        if obj_selected in ("reminder","reminders"):
+        if obj_selected in ("reminders",):
 
             reminders = self.user.get_reminders(True)
 
@@ -25,6 +25,24 @@ class Intent(BaseIntent):
 
             return self.build_template(
                 case="list_reminders",
+                resp_type=self.RESP_CLOSE,
+                text=msg,
+            )
+
+        #list_children
+        if obj_selected in ("children",):
+
+            children = self.user.get_children()
+
+            msg = "Children:"
+            counter = 1
+
+            for c in children:
+                msg+= "\n\n#%s. %s" % (counter, c.first_name)
+                counter += 1
+
+            return self.build_template(
+                case="list_children",
                 resp_type=self.RESP_CLOSE,
                 text=msg,
             )
